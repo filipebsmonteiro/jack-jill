1c6ed4bed044498421e0f03fb5fda5645f5b5030
Filipe Monteiro <filipebsmonteiro@gmail.com>
2023-11-02 14:15:38 -0300

    save attach pivot columns method

--- app/Controllers/Http/CompetitionsController.ts
+++ app/Controllers/Http/CompetitionsController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
+import { LucidRow } from '@ioc:Adonis/Lucid/Orm'
 import Competition from 'App/Models/Competition'
 import Schedule from 'App/Models/Schedule'
 import CreateValidator from 'App/Validators/Competition/CreateValidator'
@@ -7,6 +8,22 @@ import SubscribeValidator from 'App/Validators/Competition/SubscribeValidator'
 import UpdateValidator from 'App/Validators/Competition/UpdateValidator'
 
 export default class CompetitionsController {
+  private attachPivotColumns (entity: LucidRow, relationKey: string): Object {
+    const relations = entity[relationKey]?.map((relation: LucidRow) => {
+      let columns = {}
+      Object.entries(relation.$extras).map(([key, value]) => ({
+        [key.replace('pivot_', '')]: value,
+      })).forEach(element => columns = {...columns, ...element})
+
+      return { ...relation.serialize(), ...columns }
+    })
+
+    return {
+      ...entity.serialize(),
+      [relationKey]: relations,
+    }
+  }
+
   public async index ({ response }: HttpContextContract) {
     const competitions = await Competition.query()
     return response.status(200).json(competitions)
@@ -33,13 +50,49 @@ export default class CompetitionsController {
   }
 
   public async show ({ request, response }: HttpContextContract) {
-    const competition = await Competition.query()
-      .preload('schedules', (builder) => {
-        builder.orderBy('start_date')
+    let query = Competition.query().where('id', request.param('id'))
+    const relations = {
+      schedules: {
+        orderBy: 'start_date',
+      },
+      users: {
+        select: ['first_name', 'last_name'],
+      },
+    }
+
+    if (request.qs().relationships) {
+      request.qs().relationships.forEach(relation => {
+        query = query.preload(relation, (builder) => {
+          Object.entries(relations[relation])
+            .forEach(([method, value]) => {
+              if (Array.isArray(value)) {
+                builder[method](...value)
+                return
+              }
+              builder[method](value)
+            })
+        })
       })
-      .where('id', request.param('id'))
-      .firstOrFail()
-    return response.status(200).json(competition.serialize())
+    }
+
+    const competition = await query.firstOrFail()
+    // const users = competition.users?.map((user) => {
+    //   let pivot = {}
+    //   Object.entries(user.$extras).map(([key, value]) => ({
+    //     [key.replace('pivot_', '')]: value,
+    //   })).forEach(element => pivot = {...pivot, ...element})
+
+    //   return { ...user.serialize(), ...pivot }
+    // })
+
+    // return response.status(200).json({
+    //   ...competition.serialize(),
+    //   users,
+    // })
+
+    return response.status(200).json(
+      this.attachPivotColumns(competition, 'users')
+    )
   }
 
   public async update ({ request, response }: HttpContextContract) {
@@ -76,17 +129,18 @@ export default class CompetitionsController {
   }
 
   public async loadSubscribes ({ request, response }: HttpContextContract) {
-    let query = Competition.query().preload('competitors', builder => {
+    let query = Competition.query().preload('users', builder => {
       builder.select('id', 'first_name', 'last_name', 'country')
     })
 
     Object.entries(request.qs()).map(([column, value]) => query = query.where(column, value))
 
     const event = await query.firstOrFail()
-    const competitors = event.competitors.map((user) => ({
+    const competitors = event.users.map((user) => ({
       ...user.toJSON(),
       status: user.$extras.pivot_status,
       level_id: user.$extras.pivot_level_id,
+      role: user.$extras.pivot_role,
     }))
 
     return response.status(200).json({
@@ -98,17 +152,17 @@ export default class CompetitionsController {
   public async subscribe ({ request, response }: HttpContextContract) {
     const { id, userId } = await request.validate(SubscribeValidator)
     let competition = await Competition.findOrFail(id)
-    await competition.related('competitors').attach([userId])
+    await competition.related('users').attach([userId])
 
     competition = await Competition.query()
-      .preload('competitors', query => {
+      .preload('users', query => {
         query.select('id', 'first_name', 'last_name', 'country')
           .wherePivot('user_id', userId)
       })
       .where('id', id)
       .firstOrFail()
 
-    const users = competition.competitors.map((user) =>
+    const users = competition.users.map((user) =>
       ({...user.toJSON(), status: user.$extras.pivot_status})
     )
 
@@ -121,7 +175,7 @@ export default class CompetitionsController {
   public async unsubscribe ({ request, response }: HttpContextContract) {
     const { id, userId } = await request.validate(SubscribeValidator)
     const event = await Competition.findOrFail(id)
-    await event.related('competitors').detach([userId])
+    await event.related('users').detach([userId])
     return response.status(200).json(event.serialize())
   }
 }
