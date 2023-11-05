b0f3e3b69142d859398f8bd9f891dbc2a94fadd3
Filipe Monteiro <filipebsmonteiro@gmail.com>
2023-11-05 16:12:18 -0300

    Fallback image

--- resources/components/Samples/CompetitionsPreview.vue
+++ resources/components/Samples/CompetitionsPreview.vue
@@ -37,11 +37,13 @@ const callouts = [
       <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
         <div v-for="competition in list" :key="competition.id" class="group relative">
           <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-            <img
-              :src="`/file/${competition.image}`"
-              :alt="competition.name"
-              class="h-full w-full object-cover object-center"
-            />
+            <object data="/assets/fallback/competition.webp" type="image/png">
+              <img
+                :src="`/file/${competition.image}`"
+                :alt="competition.name"
+                class="h-full w-full object-cover object-center"
+              />
+            </object>
           </div>
           <h3 class="mt-6 text-sm text-gray-500">
             <a :href="competition.href">
