c8837729c0ea99b66d20d5bc91814bd055567c8f
Filipe Monteiro <filipebsmonteiro@gmail.com>
2023-11-14 12:23:19 -0300

    Add field on migration

--- database/migrations/1693420043209_users.ts
+++ database/migrations/1693420043209_users.ts
@@ -15,6 +15,7 @@ export default class extends BaseSchema {
       table.string('state').notNullable()
       table.string('country').notNullable()
       table.string('image').nullable()
+      table.enum('system_role', ['admin', 'competitor']).defaultTo('competitor')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
