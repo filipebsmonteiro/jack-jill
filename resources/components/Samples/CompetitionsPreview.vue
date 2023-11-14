83d359357691effdd20d600ad5eaa06b3d34beab
Filipe Monteiro <filipebsmonteiro@gmail.com>
2023-11-14 15:03:03 -0300

    Remove component style and inherit from Theme

--- resources/components/Samples/CompetitionsPreview.vue
+++ resources/components/Samples/CompetitionsPreview.vue
@@ -3,40 +3,17 @@ import { useCompetitionStore } from 'Resources/stores/competition';
 import { storeToRefs } from 'pinia'
 
 let { list, loading } = storeToRefs( useCompetitionStore() )
-
-const callouts = [
-  {
-    name: 'Desk and Office',
-    description: 'Work from home accessories',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-    href: '#',
-  },
-  {
-    name: 'Self-Improvement',
-    description: 'Journals and note-taking',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-    href: '#',
-  },
-  {
-    name: 'Travel',
-    description: 'Daily commute essentials',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-    href: '#',
-  },
-]
 </script>
 
 <template>
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-      <h2 class="text-2xl font-bold text-gray-900">Last Competitions</h2>
+      <!-- <h2 class="text-2xl font-bold text-gray-900">Last Competitions</h2> -->
+      <h2 class="text-2xl font-bold">Last Competitions</h2>
 
       <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
         <div v-for="competition in list" :key="competition.id" class="group relative">
-          <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+          <div class="relative h-80 w-full overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
             <object data="/fallback/competition.webp" type="image/png">
               <img
                 :src="`/file/${competition.image}`"
@@ -45,13 +22,14 @@ const callouts = [
               />
             </object>
           </div>
-          <h3 class="mt-6 text-sm text-gray-500">
+          <h3 class="mt-6 text-sm text-primary">
             <a :href="competition.href">
               <span class="absolute inset-0" />
               {{ competition.name }}
             </a>
           </h3>
-          <p class="text-base font-semibold text-gray-900">{{ competition.description }}</p>
+          <!-- <p class="text-base font-semibold text-gray-900">{{ competition.description }}</p> -->
+          <p class="text-base font-semibold">{{ competition.description }}</p>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,8 @@ module.exports = {
   ],
   darkMode: 'class',
   daisyui: {
-    themes: ['light', 'bumblebee', 'corporate', 'dark', 'dracula', 'valentine'],
+    themes: ['dracula', 'light', 'bumblebee', 'corporate', 'dark', 'valentine'],
+    // themes: ['light', 'bumblebee', 'corporate', 'dark', 'dracula', 'valentine'],
     darkTheme: 'dark', // name of one of the included themes for dark mode
     // base: true, // applies background color and foreground color for root element by default
     // styled: true, // include daisyUI colors and design decisions for all components
